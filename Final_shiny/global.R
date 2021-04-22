be71db0d5bc57e5d5e58f0a45c82d3afb5652ed3
Anna-Jorgensen <aejorge01@gmail.com>
2021-04-22 09:41:09 +0200

    added files for shiny

---
+++ Final_shiny/global.R
@@ -0,0 +1,9 @@
+# global file for shiny app 
+
+library("shiny")
+library("tidyverse")
+
+# need to load all the final/cleaned data we are using for the graphics.
+
+load('/data/Tidy_Covid_data.csv')
+
