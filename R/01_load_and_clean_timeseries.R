3d2911204b86fdcff298788e519041277aef592c
chrhjoh <s202770@student.dtu.dk>
2021-04-24 12:44:47 +0000

    Loaded and cleaned timeseries data with 01_load_and_clean_timerseries

---
+++ R/01_load_and_clean_timeseries.R
@@ -0,0 +1,66 @@
+
+rm(list=ls(all=TRUE))
+
+# Load Libraries ----------------------------------------------------------
+
+library(tidyverse)
+
+
+
+# Load data ---------------------------------------------------------------
+
+confirmed_global <- read_csv("data/_raw/time_series_covid19_confirmed_global.csv")
+deaths_global <- read_csv("data/_raw/time_series_covid19_deaths_global.csv")
+recovered_global <- read_csv("data/_raw/time_series_covid19_recovered_global.csv")
+
+
+
+# Wrangle data ------------------------------------------------------------
+
+# Pivot data
+confirmed_global <- confirmed_global %>%
+  pivot_longer(cols = matches("\\d+/\\d+/\\d+"),
+               names_to = "Date",
+               values_to = "Confirmed")
+
+deaths_global <- deaths_global %>%
+  pivot_longer(cols = matches("\\d+/\\d+/\\d+"),
+               names_to = "Date",
+               values_to = "Deaths")
+
+recovered_global <- recovered_global %>%
+  pivot_longer(cols = matches("\\d+/\\d+/\\d+"),
+               names_to = "Date",
+               values_to = "Recovered")
+
+# Generate counts only on country basis (sum Province/state)
+confirmed_global_country <- confirmed_global %>%
+  group_by(`Country/Region`, Date) %>%
+  summarise(Confirmed = sum(Confirmed))
+
+deaths_global_country <- deaths_global %>%
+  group_by(`Country/Region`, Date) %>%
+  summarise(Deaths = sum(Deaths))
+
+recovered_global_country <- recovered_global %>%
+  group_by(`Country/Region`, Date) %>%
+  summarise(Recovered = sum(Recovered))
+
+# Join time series for countries
+combined_timeseries_country <- confirmed_global_country %>%
+  left_join(deaths_global_country, by = c("Country/Region", "Date")) %>%
+  left_join(recovered_global_country, by = c("Country/Region", "Date"))
+
+# Join time series on province level
+combined_timeseries_province <- confirmed_global %>%
+  left_join(deaths_global, by = c("Country/Region", "Date", "Province/State")) %>%
+  left_join(recovered_global, by = c("Country/Region", "Date", "Province/State")) %>%
+  select(-ends_with(c('.x', '.y')))
+
+
+# Write data --------------------------------------------------------------
+combined_timeseries_country %>%
+  write_csv("data/01_timeseries_country.csv'´")
+
+combined_timeseries_province %>%
+  write_csv("data/01_timeseries_province.csv")
--- R/placeholder.R
+++
@@ -1,1 +0,0 @@
-# This is a placeholder script to get folder into github
