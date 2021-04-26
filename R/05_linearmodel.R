b0f810ec9e0eb881c8ce266fb2ba2b7cc346fbe3
Anna-Jorgensen <aejorge01@gmail.com>
2021-04-26 20:41:19 +0200

    working on a visualization... still not finished

--- R/05_linearmodel.R
+++ R/05_linearmodel.R
@@ -1,4 +1,4 @@
-rm(list=ls(all=TRUE))
+rm(list=ls(all=TRUE)) 
 
 # Load Libraries ----------------------------------------------------------
 library(tidyverse)
@@ -32,12 +32,14 @@ model_data <- latest_date_data %>%
                        data = .x,
                        family = gaussian()
                        ))) 
-model_data
 
-model_data %>% 
+
+model_data <-
+  model_data %>% 
   mutate(mdl_tidy = purrr::map(mdl, tidy, conf.int = TRUE)) %>% 
   unnest(mdl_tidy)
 
+model_data
 
 ggplot(latest_date_data, aes(`Pop%_above65`, Deaths_per_100k_citizen)) +
   geom_point(aes(color = factor(IncomeGroup))) +
@@ -49,6 +51,15 @@ ggplot(latest_date_data, aes(Age_median, Deaths_per_100k_citizen)) +
   geom_smooth(method ="lm",aes(color = IncomeGroup),se=F) +
   facet_wrap(IncomeGroup ~ .,scale="free_x")
 
+# working on this....
+model_data %>%
+  ggplot(aes(x=estimate,
+             y=IncomeGroup))+
+  geom_vline(xintercept = 0, linetype="dashed")+
+  geom_point()+
+  geom_errorbarh(aes(xmin=conf.low,xmax=conf.high))
 
+
+  
 
 
