45249a22a06a468e3000cb6f7a82182a6d4c3192
hlund08 <s174422@student.dtu.dk>
2021-04-26 10:35:02 +0200

    playing around

--- R/05_linearmodel.R
+++ R/05_linearmodel.R
@@ -8,6 +8,8 @@ library(patchwork)
 library(ggrepel)
 library(cowplot)
 source("R/99_functions.R")
+library(stringr)
+library(purrr)
 
 
 # Load Data ---------------------------------------------------------------
@@ -23,26 +25,38 @@ latest_date_data <- get_latest_date_data(timeseries_data)
 
 # Select explanatory variable (pop over 65)
 
-model_data <- latest_date_data %>% 
+latest_date_data %>% 
   select(Region, 
          Deaths_per_100k_citizen, 
          `Pop%_above65`
          ) %>% 
-  group_by(Region) %>% 
-  nest() %>% 
-  ungroup()
+  group_by(Region) %>%
+  nest %>% 
+  ungroup
+
+model_data$data[[1]]
+
+
+model_data %>% 
+  map(data, ~lm(Deaths_per_100k_citizen ~ `Pop%_above65`,
+                              data = .x,
+                              family = gaussian()))
+
+model_data  %>%
+  mutate(mdl = map(conf.int = TRUE,data,
+                   ~lm(Deaths_per_100k_citizen ~ `Pop%_above65`, 
+                       data = .x,
+                       family = gaussian())))
+
+country_model <- function(df) {
+  lm(Deaths_per_100k_citizen ~ `Pop%_above65`, data = df)
+}
+
+models <- map(model_data$data, country_model)
 
 model_data_lm <- model_data %>% 
-  mutate(mdl = map((conf.int = TRUE,data,~glm(Deaths_per_100k_citizen ~ `Pop%_above65`, 
-                                              data = .x, family = binomial(link = "logit"))), tidied = map(mdl, tidy)) %>% 
-           unnest(tidied)
-
-
-gravier_sample_mdl <- gravier_sample %>% 
-  mutate(mdl = map((conf.int = TRUE,data,~glm(outcome ~ log2_expr_level, data = .x, family = binomial(link = "logit"))), tidied = map(mdl, tidy)) %>% 
-           unnest(tidied)
-
-
+  mutate(model = map(data, country_model))
+model_data
 
 
 
