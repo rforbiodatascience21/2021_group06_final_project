8e36ff64cedb5047e57569d373842e23680d282d
chrhjoh <s202770@student.dtu.dk>
2021-04-26 14:03:09 +0000

    Added 00_doit.R

---
+++ R/00_doit.R
@@ -0,0 +1,7 @@
+# Run all scripts ---------------------------------------------------------
+source(file = "R/01_load_and_clean_timeseries.R")
+source(file = "R/02_load_and_clean_country_data.R")
+source(file = "R/03_augment_data.R")
+source(file = "R/04_pca.R")
+source(file = "R/05_linearmodel.R")
+source(file = "R/06_eda_christian.R")
