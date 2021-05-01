e9d42b43d8952fc85948bf16080bb5fd054b7f90
Anna-Jorgensen <aejorge01@gmail.com>
2021-05-01 21:36:13 +0200

    Added a linear model. And fixed ggplot errors

--- R/05_linearmodel.R
+++ R/05_linearmodel.R
@@ -1,11 +1,11 @@
 rm(list=ls(all=TRUE)) 
 
 # Load Libraries ----------------------------------------------------------
-library(tidyverse)
+library("tidyverse")
 source("R/99_functions.R")
-library(stringr)
-library(purrr)
-library(broom)
+library("stringr")
+library("purrr")
+library("broom")
 
 # Load Data ---------------------------------------------------------------
 
@@ -40,25 +40,82 @@ model_data <-
   mutate(mdl_tidy = purrr::map(mdl, tidy, conf.int = TRUE)) %>% 
   unnest(mdl_tidy)
 
-model_data
+model_data 
 
 ggplot(latest_date_data, aes(`Pop%_above65`, Deaths_per_100k_citizen)) +
   geom_point(aes(color = factor(IncomeGroup))) +
   geom_smooth(method ="lm",aes(color = IncomeGroup),se=F) +
-  coord_cartesian() 
+  coord_cartesian()+
+  theme_minimal()+
+  labs(y="Deaths per 100k", x = "Median Age", color = "Income Group")
 
 ggplot(latest_date_data, aes(Age_median, Deaths_per_100k_citizen)) +
   geom_point(aes(color = factor(IncomeGroup))) +
   geom_smooth(method ="lm",aes(color = IncomeGroup),se=F) +
-  facet_wrap(IncomeGroup ~ .,scale="free_x")
+  facet_wrap(IncomeGroup ~ .,scale="free_x")+
+  theme_minimal()+
+  labs(y="Deaths per 100k", x = "Median Age", color = "Income Group")
 
 # working on this....
 model_data %>%
+  filter(term!="(Intercept)") %>%
   ggplot(aes(x=estimate,
              y=IncomeGroup))+
   geom_vline(xintercept = 0, linetype="dashed")+
   geom_point()+
   geom_errorbarh(aes(xmin=conf.low,xmax=conf.high))
+  
+  library(ggplot2)
+  td <- tidy(fit, conf.int = TRUE)
+  ggplot(td, aes(estimate, term, color = term)) +
+    geom_point() +
+    geom_errorbarh(aes(xmin = conf.low, xmax = conf.high)) +
+    geom_vline()
+
+## second model:
+
+model_data2 <- 
+  latest_date_data %>% 
+  select(Region, 
+         Confirmed_per_100k_citizen, 
+         Gdp,
+         Inequality,
+         Sex_ratio) %>% 
+  group_by(Region) %>% 
+  nest() %>% 
+  ungroup() %>%  
+  mutate(mdl = purrr::map(data,
+                          ~glm(Confirmed_per_100k_citizen ~ Gdp+Inequality+Sex_ratio, 
+                               data = .x,
+                               family = gaussian()
+                          ))) 
+
+model_data2 <-
+  model_data2 %>% 
+  mutate(mdl_tidy = purrr::map(mdl, tidy, conf.int = TRUE)) %>% 
+  unnest(mdl_tidy)
+
+# all the data 
+model_data2 %>%
+  filter(term!="(Intercept)") %>%
+  ggplot(aes(estimate, Region, color = term)) +
+  geom_point() +
+  geom_errorbarh(aes(xmin = conf.low, xmax = conf.high)) +
+  geom_vline(xintercept = 0, linetype="dashed")
+
+# Only the sig ones. 
+model_data2 %>%
+  filter(term!="(Intercept)",
+         p.value<0.05) %>%
+  ggplot(aes(estimate, Region, color = term)) +
+  geom_point() +
+  geom_errorbarh(aes(xmin = conf.low, xmax = conf.high)) +
+  geom_vline(xintercept = 0, linetype="dashed")+
+  theme_minimal()
+
+
+
+
 
 
   
