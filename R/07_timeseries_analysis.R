50b9629f825c32da99474f4ee501e45683c0f95c
JoenHaahrJensen <joen.haahr.jensen@gmail.com>
2021-05-03 09:14:08 +0000

    Updated with axis titles

--- R/07_timeseries_analysis.R
+++ R/07_timeseries_analysis.R
@@ -4,7 +4,6 @@ rm(list=ls(all=TRUE))
 # Load Libraries ----------------------------------------------------------
 
 library("tidyverse")
-library("tidyquant")
 
 augmented_timeseries <- read_csv("data/03_augmented_timeseries.csv")
 
@@ -51,10 +50,15 @@ augmented_timeseries %>%
   geom_point()+
   scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y") +
   theme_minimal()+
-  theme(axis.text.x = element_text(angle=45, hjust = 1))
+  theme(axis.text.x = element_text(angle=45, hjust = 1))+
+  labs(title = "How many countries were in a wave at any given date",
+       subtitle = "Number of countries with an increase of 10% in confirmed cases over a 7 day period",
+       x="Date",
+       y="Number of countries")
 
 # plotting the mean (14-day mean) number of countries that actively have a wave by region
 augmented_timeseries %>% 
+  drop_na(Region) %>% 
   group_by(Date,Region) %>%
   filter(Wave_status == "Wave") %>% 
   count(name = "no_waves")%>% 
@@ -67,45 +71,60 @@ augmented_timeseries %>%
   scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y") +
   theme_minimal()+
   theme(axis.text.x = element_text(angle=45, hjust = 1))+
-  facet_wrap(~Region)
+  facet_wrap(~Region)+
+  labs(title = "How many countries in a region were in a wave at any given date",
+       subtitle = "Number of countries with an increase of 10% in confirmed cases over a 7 day period",
+       x="Date",
+       y="Number of countries")
 
-augmented_timeseries2 <- augmented_timeseries %>% 
-  filter(`Country/Region` == "Denmark")
+### select a country
+selected_country <-  "Sweden"
 
-augmented_timeseries2 %>% 
+augmented_timeseries_single_country <- augmented_timeseries %>% 
+  filter(`Country/Region` == selected_country)
+
+augmented_timeseries_single_country %>% 
   drop_na() %>% 
   ggplot(aes(x = Date, y = Rolling_mean_deaths, color = Wave_status))+
   geom_point()+
   scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y") +
   theme_minimal()+
-  theme(axis.text.x = element_text(angle=45, hjust = 1))
+  theme(axis.text.x = element_text(angle=45, hjust = 1))+
+  labs(title = str_c("Identifying waves in ",selected_country),
+       subtitle = str_c("Waves are identified as ",
+                        (increase_factor-1)*100,
+                        "% growth over a period of ",
+                        no_of_days,
+                        " days"),
+       x="Date",y="Daily number of confirmed deaths")
 
-augmented_timeseries2 %>% 
-  ggplot(aes(x = Date))+
-  geom_line(aes(y = New_confirmed),color = "blue")+
-  geom_line(aes(y = New_deaths),color = "red")+
-  geom_line(aes(y = New_recovered),color = "green")+
-  scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y")+
-  scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y") +
-  theme_minimal()+
-  theme(axis.text.x = element_text(angle=45, hjust = 1))
-
-augmented_timeseries2 %>% 
+augmented_timeseries_single_country %>% 
   ggplot(aes(x = Date,
-             y = Case_fatality))+
+             y = Case_fatality*100))+
   geom_point()+
   scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y") +
   theme_minimal()+
-  theme(axis.text.x = element_text(angle=45, hjust = 1))
+  theme(axis.text.x = element_text(angle=45, hjust = 1))+
+  labs(title = "Does the case-fatality change over time?",
+       subtitle = str_c("Cummulative case fatility in ",
+                        selected_country,
+                        " over time")
+       ,x="Date",
+       y="Case fatality (%)")
 
-
-augmented_timeseries2 %>% 
+### ISSUE, want to have dual axis, one for case fatality and one for new_case
+augmented_timeseries_single_country %>% 
   ggplot(aes(x = Date))+
-  geom_point(aes(y = Case_fatality*100), color = "black") +
-  geom_point(aes(y = Rolling_case_fatality*100), color = "red") +
-  geom_point(aes(y = log10(New_confirmed)), color = "Green")+
+  geom_point(aes(y = Case_fatality*100, color = "Cummulative Case Fatility")) +
+  geom_point(aes(y = Rolling_case_fatality*100, color = "Rolling Case Fatility")) +
+  geom_point(aes(y = log10(New_confirmed),color = "Log10 of daily cases"))+
   scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y") +
+  scale_y_continuous(
+    name = "Case Fatality (%)",
+    sec.axis = sec_axis(~.*1, name="Log10 of daily confirmed cases")
+  )+ 
   theme_minimal()+
-  theme(axis.text.x = element_text(angle=45, hjust = 1))
-
-
+  theme(axis.text.x = element_text(angle=45, hjust = 1),legend.position = "bottom")+
+  labs(title = "Case fatality ratio spike right after a drop of confirmed cases",
+       subtitle = "Case fatility rates differ over time",
+       x = "Date")
