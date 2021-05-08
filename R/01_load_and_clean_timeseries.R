fc8c828607f2ffc45cd657442e0d2ade87a82641
JoenHaahrJensen <joen.haahr.jensen@gmail.com>
2021-05-08 14:38:10 +0000

    Adding DRY version of the script

--- R/01_load_and_clean_timeseries.R
+++ R/01_load_and_clean_timeseries.R
@@ -14,36 +14,6 @@ recovered_global <- read_csv("data/_raw/time_series_covid19_recovered_global.csv
 
 # Wrangle data ------------------------------------------------------------
 
-covid_data <- tribble(
-  ~variable_name, ~file_path,
-  "Confirmed",    "data/_raw/time_series_covid19_confirmed_global.csv",
-  "Deaths",       "data/_raw/time_series_covid19_deaths_global.csv",
-  "Recovered",    "data/_raw/time_series_covid19_recovered_global.csv"
-)
-
-covid_data <- covid_data %>% 
-  mutate(raw_data = purrr::map(file_path, ~read_csv(.)))
-
-pivot_data <- function(df, var_name){
-  df %>%
-    pivot_longer(cols = matches("\\d+/\\d+/\\d+"),
-                 names_to = "Date",
-                 values_to = var_name)
-}
-
-covid_data <- covid_data %>% 
-  mutate(pivoted_data = purrr::map2(raw_data, variable_name, pivot_data))
-%>% 
-  pluck("pivoted_data") %>% 
-  purrr::reduce(left_join, by = c("Province/State","Country/Region","Lat","Long","Date"))
-
-covid_data <- covid_data %>% 
-  group_by(`Country/Region`, Date) %>%
-  summarise(Confirmed = sum(Confirmed),
-            Deaths = sum(Deaths),
-            Recovered = sum(Recovered)) %>% 
-  mutate(Date = lubridate::mdy(Date))
-
 # Pivot data
 confirmed_global <- confirmed_global %>%
   pivot_longer(cols = matches("\\d+/\\d+/\\d+"),
@@ -91,3 +61,39 @@ combined_timeseries_country <- combined_timeseries_country %>%
 # Write data --------------------------------------------------------------
 combined_timeseries_country %>%
   write_csv("data/01_timeseries_country.csv")
+
+# Alternate  way ------------------------------------------------------------
+
+
+covid_data <- tribble(
+  ~variable_name, ~file_path,
+  "Confirmed",    "data/_raw/time_series_covid19_confirmed_global.csv",
+  "Deaths",       "data/_raw/time_series_covid19_deaths_global.csv",
+  "Recovered",    "data/_raw/time_series_covid19_recovered_global.csv"
+)
+
+#load data
+covid_data <- covid_data %>% 
+  mutate(raw_data = purrr::map(file_path, ~read_csv(.)))
+
+#define a function for pivoting the data to a long format
+pivot_data <- function(df, var_name){
+  df %>%
+    pivot_longer(cols = matches("\\d+/\\d+/\\d+"),
+                 names_to = "Date",
+                 values_to = var_name)
+}
+
+#pivot the data and join the pivoted dataframes
+covid_data <- covid_data %>% 
+  mutate(pivoted_data = purrr::map2(raw_data, variable_name, pivot_data)) %>% 
+  pluck("pivoted_data") %>% 
+  purrr::reduce(left_join, by = c("Province/State","Country/Region","Lat","Long","Date"))
+
+#Sum up variable on country level and reformat date.
+covid_data <- covid_data %>% 
+  group_by(`Country/Region`, Date) %>%
+  summarise(Confirmed = sum(Confirmed),
+            Deaths = sum(Deaths),
+            Recovered = sum(Recovered)) %>% 
+  mutate(Date = lubridate::mdy(Date))
