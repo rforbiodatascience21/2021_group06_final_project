f3cac1b7c90aba21411a5cdd8f31fea1edc95c6c
chrhjoh <s202770@student.dtu.dk>
2021-05-10 18:46:04 +0000

    removed unnecessary package cowplot from pca

--- R/04_pca.R
+++ R/04_pca.R
@@ -6,9 +6,7 @@ library("tidyverse")
 library("broom")
 library("patchwork")
 library("ggrepel")
-library("cowplot")
 source("R/99_functions.R")
-
 
 # Load Data ---------------------------------------------------------------
 timeseries_data <- read_csv("data/03_augmented_timeseries.csv",
